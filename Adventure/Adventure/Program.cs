fcf1b6bb10aeeb8a6f53272e1560e777e5897c1c
AreannaW <92953124+AreannaW@users.noreply.github.com>
2021-10-27 21:05:00 -0500

    Add files via upload

---
+++ Adventure/Adventure/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Adventure
+{
+    class Program
+    {
+        static void Main()
+        {
+                String CharacterName = "Beth";
+
+                Console.WriteLine("Areys Creations!");
+                Console.WriteLine(" A girl that creates different things to explore her creativiety.");
+                Console.WriteLine("What is your name?");
+            CharacterName = Console.ReadLine();
+            Console.WriteLine("Great to meet you " + CharacterName);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Adventure/Adventure/heyyy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Adventure
+{
+    class adding
+     static string Add(int x,int y)
+    {
+        int sum = x + y;
+        return String.Format($"{sum} is the result of {x} plus {y}");
+    }
+}
